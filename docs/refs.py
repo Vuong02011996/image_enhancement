64e5f428288a24f50407986a9f63f6818e39609b
Vuong <vuonglv@greenglobal.vn>
2020-07-17 10:58:54 +0700

    remove models, add .gitignore, add docs

---
+++ docs/refs.py
@@ -0,0 +1,30 @@
+"""
+Implementation of Efficient Subpixel Convolution Network (ESPCN)
+Author: Zixuan Zhang
+Date: 5/30/2020
+Reference: https://arxiv.org/abs/1609.05158
+"""
+
+"""
+A simple VGG-style classification network used as discriminator
+Author: Zixuan Zhang
+Date: 5/30/2020
+Reference: https://arxiv.org/pdf/1609.04802.pdf
+"""
+'''
+discriminator
+'''
+
+"""
+Implementation of RRDBNet
+Author: Zixuan Zhang
+Date: 5/30/2020
+Reference: https://arxiv.org/pdf/1809.00219.pdf
+"""
+
+"""
+Implementation of SRResNet
+Author: Zixuan Zhang
+Date: 5/30/2020
+Reference: https://arxiv.org/pdf/1609.04802.pdf
+"""
